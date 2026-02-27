ce89a67d1077a5cf8f9446c79296f746437de121
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2026-02-27 18:31:29 +0200

    fix: remove orphaned migration files not tracked by drizzle

--- db/migrations/0004_add_personal_info.sql
+++
@@ -1,5 +0,0 @@
--- Add personalInfo field to surveys table
--- This field stores an array of personal information types to collect (e.g., email, name, phone number)
-
-ALTER TABLE "surveys" ADD COLUMN IF NOT EXISTS "personal_info" TEXT[] DEFAULT '{}';
-
